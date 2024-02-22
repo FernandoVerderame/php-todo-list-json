5b94f887f170ec0c1463068c78ae19df1c8df54a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-22 17:50:49 +0100

    added new task into index.php

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -4,8 +4,29 @@ $source_path = __DIR__ . '/../../database/tasks.json';
 
 $json_data = file_get_contents($source_path);
 
+
+$tasks = $json_data;
+
+$new_task = $_POST['task'] ?? '';
+
+if ($new_task) {
+
+    $tasks = json_decode($tasks, true);
+
+    $new_task = array(
+        "id" => time(),
+        "text" => $_POST['task'],
+        "done" => false
+    );
+
+    $tasks[] = $new_task;
+
+    $tasks = json_encode($tasks);
+}
+
+
 header ('Content-Type: application/json');
 
-echo $json_data;
+echo $tasks;
 
 ?>
