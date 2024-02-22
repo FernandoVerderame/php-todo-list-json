a6973318fa8f24b760082460f352943d0478bd47
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-22 16:34:32 +0100

    added dinamic data by json

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,30 +1,11 @@
 <?php
 
-$tasks = [
-    [
-        "id" => 1,
-        "text" => "HTML",
-        "done" => false
-    ],
-    [
-        "id" => 2,
-        "text" => "CSS",
-        "done" => false
-    ],
-    [
-        "id" => 3,
-        "text" => "Responsive design",
-        "done" => false
-    ],
-    [
-        "id" => 4,
-        "text" => "Javascript",
-        "done" => false
-    ]
-];
+$source_path = __DIR__ . '/../../database/tasks.json';
+
+$json_data = file_get_contents($source_path);
 
 header ('Content-Type: application/json');
 
-echo json_encode($tasks);
+echo $json_data;
 
 ?>
