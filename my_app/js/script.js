79e000f8de903f459ce06c0ed97740a8ef761835
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-22 16:24:59 +0100

    added tasks list by axios

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,32 +1,19 @@
 const { createApp } = Vue;
 
+const endpoint = 'http://localhost/Boolean/php-todo-list-json/api/tasks/';
+
 const app = createApp({
     name: 'Todo List PHP',
 
     data: () => ({
-        tasks: [
-            {
-                "id": 1,
-                "text": "HTML",
-                "done": false
-            },
-            {
-                "id": 2,
-                "text": "CSS",
-                "done": false
-            },
-            {
-                "id": 3,
-                "text": "Responsive design",
-                "done": false
-            },
-            {
-                "id": 4,
-                "text": "Javascript",
-                "done": false
-            }
-        ]
-    })
+        tasks: []
+    }),
+
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+        })
+    }
 });
 
 app.mount('#app');
