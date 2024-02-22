51cf5594cec52e87211e1cce4b266b05bfaf9263
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-22 18:11:15 +0100

    added comments into index.php

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,34 +1,43 @@
 <?php
 
+// JSON path
 $source_path = __DIR__ . '/../../database/tasks.json';
 
 $json_data = file_get_contents($source_path);
 
-
+// Our tasks
 $tasks = $json_data;
 
+// New task check
 $new_task = $_POST['task'] ?? '';
 
 if ($new_task) {
 
     $tasks = json_decode($tasks, true);
 
+    // New task creation
     $new_task = array(
         "id" => time(),
         "text" => $_POST['task'],
         "done" => false
     );
 
+    // New task validation
     if (!in_array($new_task, $tasks)) {
+        
+        // Add new task
         $tasks[] = $new_task;
+
     } else {
+
+        // Error message
         echo json_encode('{"error": "Si è verificato un errore"}');
     }
 
 
     $tasks = json_encode($tasks);
 
-
+    // Update JSON file
     file_put_contents($source_path, $tasks);
 }
 
